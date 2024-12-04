3a24db6c67e5ebe090e1f44fcaca5174b89d756c
Omar ♔ <o_yousaf@live.co.uk>
2024-12-04 18:34:58 +0000

    squashing bugs

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,21 +1,37 @@
 "use client";
+
 import { useState } from "react";
 import { FaBars, FaTimes } from "react-icons/fa";
 import { motion, AnimatePresence } from "framer-motion";
 import { NAV_LINKS, socialLinks } from "@/constants";
 import Image from "next/image";
 
+const variants = {
+  mobileMenu: {
+    hidden: { x: "100%" },
+    visible: { x: 0, transition: { duration: 0.4, ease: "easeInOut" } },
+    exit: { x: "100%", transition: { duration: 0.3, ease: "easeInOut" } },
+  },
+  menuItem: {
+    hidden: { opacity: 0, y: 20 },
+    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
+  },
+  socialIcon: {
+    hover: {
+      scale: 1.2,
+      rotate: -5,
+      transition: { type: "spring", stiffness: 200, damping: 10 },
+    },
+  },
+};
+
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
 
   const handleScroll = (sectionId) => {
     const targetSection = document.getElementById(sectionId);
-    targetSection?.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-    setIsOpen(false);
-    document.body.style.overflow = "auto";
+    targetSection?.scrollIntoView({ behavior: "smooth", block: "start" });
+    closeMenu();
   };
 
   const toggleMenu = () => {
@@ -23,19 +39,26 @@ export default function Navbar() {
     document.body.style.overflow = isOpen ? "auto" : "hidden";
   };
 
-  const mobileMenuVariants = {
-    hidden: { x: "100%" },
-    visible: {
-      x: 0,
-      transition: { duration: 0.4, ease: "easeInOut", staggerChildren: 0.1 },
-    },
-    exit: { x: "100%", transition: { duration: 0.3, ease: "easeInOut" } },
+  const closeMenu = () => {
+    setIsOpen(false);
+    document.body.style.overflow = "auto";
   };
 
-  const menuItemVariants = {
-    hidden: { opacity: 0, y: 20 },
-    visible: { opacity: 1, y: 0, transition: { duration: 0.3 } },
-  };
+  const renderSocialIcons = (iconSize) =>
+    socialLinks.map(({ href, icon: Icon }, index) => (
+      <motion.a
+        key={index}
+        href={href}
+        target="_blank"
+        rel="noreferrer"
+        className="text-beige hover:text-tan"
+        aria-label={`Social media link ${index + 1}`}
+        whileHover="hover"
+        variants={variants.socialIcon}
+      >
+        <Icon size={iconSize} />
+      </motion.a>
+    ));
 
   return (
     <nav className="fixed top-0 w-full bg-black text-beige shadow-md z-50">
@@ -53,13 +76,11 @@ export default function Navbar() {
             height={50}
             priority
           />
-          {/* Drainage Hero */}
         </button>
 
         {/* Hamburger Icon */}
         <button
           aria-label="Toggle menu"
-          aria-expanded={isOpen}
           onClick={toggleMenu}
           className="md:hidden"
         >
@@ -67,7 +88,7 @@ export default function Navbar() {
         </button>
 
         {/* Desktop Navbar Links */}
-        <ul className="hidden md:flex space-x-6 text-tan md:text-beige">
+        <ul className="hidden md:flex space-x-6">
           {NAV_LINKS.map(({ id, label }) => (
             <li key={id}>
               <button
@@ -82,20 +103,7 @@ export default function Navbar() {
         </ul>
 
         {/* Desktop Social Media Icons */}
-        <div className="hidden md:flex space-x-4">
-          {socialLinks.map(({ href, icon: Icon }, index) => (
-            <a
-              key={index}
-              href={href}
-              target="_blank"
-              rel="noreferrer"
-              className="text-beige hover:text-tan transition-all duration-300 ease-in-out"
-              aria-label={`Social media link ${index + 1}`}
-            >
-              <Icon size={24} />
-            </a>
-          ))}
-        </div>
+        <div className="hidden md:flex space-x-4">{renderSocialIcons(24)}</div>
       </div>
 
       {/* Mobile Menu */}
@@ -105,7 +113,7 @@ export default function Navbar() {
             initial="hidden"
             animate="visible"
             exit="exit"
-            variants={mobileMenuVariants}
+            variants={variants.mobileMenu}
             className="fixed top-0 left-0 w-full h-screen bg-black text-beige flex flex-col z-50"
           >
             {/* Close Button */}
@@ -118,12 +126,9 @@ export default function Navbar() {
             </button>
 
             {/* Navbar Links */}
-            <motion.ul
-              className="flex flex-col items-center justify-center flex-grow space-y-8"
-              variants={mobileMenuVariants}
-            >
+            <motion.ul className="flex flex-col items-center justify-center flex-grow space-y-8">
               {NAV_LINKS.map(({ id, label }) => (
-                <motion.li key={id} variants={menuItemVariants}>
+                <motion.li key={id} variants={variants.menuItem}>
                   <button
                     onClick={() => handleScroll(id)}
                     className="text-2xl font-bold uppercase mt-3 hover:text-tan transition-all duration-300 ease-in-out"
@@ -135,24 +140,14 @@ export default function Navbar() {
               ))}
             </motion.ul>
 
-            {/* Social Media Icons */}
+            {/* Mobile Social Media Icons */}
             <motion.div
               className="w-full flex justify-center space-x-6 mb-20"
-              variants={mobileMenuVariants}
+              initial="hidden"
+              animate="visible"
+              exit="exit"
             >
-              {socialLinks.map(({ href, icon: Icon }, index) => (
-                <motion.a
-                  key={index}
-                  href={href}
-                  target="_blank"
-                  rel="noreferrer"
-                  className="text-beige hover:text-tan transition-all duration-300 ease-in-out"
-                  aria-label={`Social media link ${index + 1}`}
-                  variants={menuItemVariants}
-                >
-                  <Icon size={32} />
-                </motion.a>
-              ))}
+              {renderSocialIcons(32)}
             </motion.div>
           </motion.div>
         )}

114c7a231249613d37a7ff9be0a86768628e3b36
Omar ♔ <o_yousaf@live.co.uk>
2024-12-08 12:11:50 +0000

    squashing bugs

--- app/page.jsx
+++ app/page.jsx
@@ -2,6 +2,7 @@ import Hero from "@/components/Hero";
 import Services from "@/components/Services";
 import About from "@/components/About";
 import Contact from "@/components/Contact";
+import Snow from "@/components/Snow";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <About />
       <Services />
       <Contact />
+      <Snow />
     </main>
   );
 }

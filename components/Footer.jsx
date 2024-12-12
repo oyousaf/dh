c398b6769e07d6e4dc5285a99c32f83b7f1eff2c
Omar ♔ <o_yousaf@live.co.uk>
2024-12-12 16:16:19 +0000

    optimising SEO

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -14,19 +14,41 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
 
   return (
-    <footer className="bg-black py-6 text-center">
-      <p className="text-lg">&copy; {currentYear} Drainage Hero</p>
-      <p>All rights reserved</p>
-      <div className="flex justify-center space-x-4 mt-4">
-        {socialLinks.map(({ href, icon: Icon }, index) => (
+    <footer className="bg-black py-6 text-center" role="contentinfo">
+      <p className="text-xl p-6">
+        &copy; {currentYear} Drainage Hero. All rights reserved.
+      </p>
+
+      <div className="md:text-lg text-md">
+        <address className="not-italic">
+          <p>Hawkingcroft Rd, Horbury, Wakefield WF4 6AH</p>
+          <p className="mt-2">
+            <a
+              href="tel:+447722128784"
+              className="hover:text-secondary"
+              title="Call Drainage Hero at 07722128784"
+            >
+              07722128784
+            </a>
+          </p>
+        </address>
+      </div>
+
+      <div
+        className="flex justify-center space-x-4 mt-6"
+        aria-label="Social media links"
+      >
+        {socialLinks.map(({ href, icon: Icon, name }, index) => (
           <motion.a
             key={index}
             href={href}
             target="_blank"
-            rel="noreferrer"
+            rel="noopener noreferrer"
             className="hover:text-secondary"
             whileHover="hover"
             variants={socialIconVariants}
+            aria-label={`Follow us on ${name}`}
+            title={`Follow Drainage Hero on ${name}`}
           >
             <Icon size={30} />
           </motion.a>

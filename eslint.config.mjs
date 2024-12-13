ea7100ad24959bdf17fa8e2ba49b45fec04d20e6
Omar ♔ <o_yousaf@live.co.uk>
2024-12-13 13:51:33 +0000

    squashing bugs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,10 +4,22 @@ import { fileURLToPath } from "url";
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-export default {
-  extends: ["next/core-web-vitals"],
-  parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: "module",
+export default [
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    languageOptions: {
+      parserOptions: {
+        ecmaVersion: 2020,
+        sourceType: "module",
+      },
+    },
+    plugins: [],
+    rules: {
+    },
+    settings: {},
   },
-};
+  {
+    files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+    extends: ["next/core-web-vitals"],
+  },
+];
